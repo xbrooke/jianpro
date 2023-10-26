81ac1bb0b4f6fdd1da570cec1c3c87db4c8df8b1
xbrooke <107459849+xbrooke@users.noreply.github.com>
2023-10-26 11:02:44 +0800

    Update custom-giscus-style.js

--- assets/js/custom-giscus-style.js
+++ assets/js/custom-giscus-style.js
@@ -1,1 +1,11 @@
+// 通过等待文档加载完成，确保 "giscus" 插件已加载
+document.addEventListener("DOMContentLoaded", function () {
+    // 获取 "giscus" 插件的评论框元素
+    const commentBox = document.querySelector(".giscus-comment-box");
+
+    // 添加自定义样式
+    if (commentBox) {
+        commentBox.style.background = "white";
+    }
+});
 
